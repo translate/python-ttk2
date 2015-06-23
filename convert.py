3633824190514bd955c43278d0557d6f67dd766b
Thibaut Vuillemin <contact@thibautvuillemin.com>
2015-06-23 23:05:10 +0200

    Move the conversion algorithm into a separate function.

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
-from ttk2.formats import guess_format
+from ttk2.conversion import convert
+
 
 def main():
 	import sys
@@ -11,48 +12,7 @@ def main():
 	arguments.add_argument("outfile", nargs=1)
 	arguments.add_argument("infile", nargs="+")
 	args = arguments.parse_args(sys.argv[1:])
-
-	instores = []
-	for path in args.infile:
-		cls = guess_format(path)
-		store = cls()
-		with open(path, "r") as f:
-			store.read(f, lang="todo")
-		instores.append(store)
-
-	if args.template:
-		path = args.template[0]
-		cls = guess_format(path)
-		template = cls()
-		with open(path, "r") as f:
-			template.read(f, lang="todo")
-	else:
-		template = None
-
-	# Create the outgoing store
-	outstore = guess_format(args.outfile[0])()
-	for store in instores:
-		for unit in store.units:
-			outstore.units.append(unit)
-
-	if template:
-		tunits = []
-		for unit in template.units:
-			unit.propkey = unit.key
-			unit.key = unit.value
-			unit.value = ""
-			tunits.append(unit)
-
-		for unit in outstore.units:
-			for tunit in tunits:
-				if unit.key == tunit.propkey:
-					tunit.value = unit.value
-
-		outstore.units = tunits
-
-	with open(args.outfile[0], "w") as f:
-		f.write(outstore.serialize())
-
+	convert(args.outfile[0], args.infile, template=args.template)
 
 if __name__ == "__main__":
 	main()
---
+++ ttk2/conversion.py
@@ -0,0 +1,54 @@
+from ttk2.formats import guess_format
+import logging
+
+def convert(outfile, infile_list, template=None):
+	"""
+	Parse a list of config files, merge them and write the result in the desired output file.
+
+	In the beginning, check that infile_list is not a single string, otherwise we would iterate
+	over the string characters. If this is indeed a string, log a warning and create a list with
+	this one element to keep going.
+	"""
+	if isinstance(infile_list, str):
+		infile_list = [infile_list]
+		logging.warning("Convert should be provided with a LIST of input files.")
+
+	instores = []
+	for path in infile_list:
+		cls = guess_format(path)
+		store = cls()
+		with open(path, "r") as input_file:
+			store.read(input_file, lang="todo")
+		instores.append(store)
+
+	if template:
+		path = template[0]
+		cls = guess_format(path)
+		template_cls = cls()
+		with open(path, "r") as template_file:
+			template_file.read(template_file, lang="todo")
+	else:
+		template_cls = None
+
+	outstore = guess_format(outfile)()
+	for store in instores:
+		for unit in store.units:
+			outstore.units.append(unit)
+
+	if template_cls:
+		tunits = []
+		for unit in template_cls.units:
+			unit.propkey = unit.key
+			unit.key = unit.value
+			unit.value = ""
+			tunits.append(unit)
+
+		for unit in outstore.units:
+			for tunit in tunits:
+				if unit.key == tunit.propkey:
+					tunit.value = unit.value
+
+		outstore.units = tunits
+
+	with open(outfile, "w") as output_file:
+		output_file.write(outstore.serialize())
